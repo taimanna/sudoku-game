d1176c8235eb163ea946a82ae2f1e0fccf69d7f0
taimanna <nhannt.gm@gmail.com>
2024-03-01 23:31:56 +0700

    [add] auto candidate

--- src/components/SudokuGrid/index.js
+++ src/components/SudokuGrid/index.js
@@ -15,7 +15,6 @@ const SudokuGrid = () => {
   const [wrongCellPosition, setWrongCellPosition] = useState([])
   const [selectedNumber, setSelectedNumber] = useState(null)
   const [isPopup, setIsPopup] = useState(false)
-  const [isAutoCandidate, setIsAutoCandidate] = useState(false)
   const numpad = [
     ['1', '2', '3'],
     ['4', '5', '6'],
@@ -75,45 +74,63 @@ const SudokuGrid = () => {
     setDifficult(selectedDifficult)
   }
 
+  const resetCandidates = () => {
+    // reset old candidate
+    const candidateTag = document.querySelectorAll('div[data-candidate]')
+    candidateTag.forEach((tag) => {
+      tag.classList.remove('opacity-full')
+    })
+  }
+
   const getCandidates = () => {
-    const stringGrid = sudokuUtils.sudoku.board_grid_to_string(sudokuGrid)
-    const candidatesFromUtils = sudokuUtils.sudoku.get_candidates(stringGrid)
-    const tdTags = document.querySelectorAll('.sudoku-table td:not(.original-cell) .candidate-container')
+    const candidateCheckbox = document.getElementById('candidate')
 
-    if (candidatesFromUtils) {
-      const flatSudokuGrid = sudokuGrid.flat(Infinity)
-      const flatCandidates = candidatesFromUtils.flat(Infinity)
-      const candidates = flatCandidates.filter((value, i) => value !== flatSudokuGrid[i])
+    if (candidateCheckbox.checked) {
+      const stringGrid = sudokuUtils.sudoku.board_grid_to_string(sudokuGrid)
+      const candidatesFromUtils = sudokuUtils.sudoku.get_candidates(stringGrid)
+      const tdTags = document.querySelectorAll('.sudoku-table td:not(.original-cell) .candidate-container')
 
-      // reset old candidate
-      const candidateTag = document.querySelectorAll('div[data-candidate]')
-      candidateTag.forEach((tag) => {
-        tag.classList.remove('opacity-full')
-      })
+      if (candidatesFromUtils) {
+        const flatSudokuGrid = sudokuGrid.flat(Infinity)
+        const flatCandidates = candidatesFromUtils.flat(Infinity)
+        const candidates = flatCandidates.filter((value, i) => value !== flatSudokuGrid[i])
 
-      // auto fill candidate to sudoku grid
-      candidates.forEach((candidateString, i) => {
-        const candidateArr = candidateString.split('')
-        candidateArr.forEach((candidate) => {
-          tdTags[i].querySelector(`div[data-candidate="${candidate}"]`).classList.add('opacity-full')
+        // reset candidate
+        resetCandidates()
+
+        // auto fill candidate to sudoku grid
+        candidates.forEach((candidateString, i) => {
+          const candidateArr = candidateString.split('')
+          candidateArr.forEach((candidate) => {
+            tdTags[i].querySelector(`div[data-candidate="${candidate}"]`).classList.add('opacity-full')
+          })
         })
-      })
+      }
+    } else {
+      // reset candidate
+      resetCandidates()
     }
   }
 
   const handleClickNumpadCell = (e) => {
+    const target = e.target ? e.target : e
+
     if (prevNumpadCell) {
-      if (prevNumpadCell === e.target) {
-        e.target.classList.toggle('selected-cell')
+      console.log('if1')
+      if (prevNumpadCell === target) {
+        console.log('if2')
+        target.classList.toggle('selected-cell')
       } else {
+        console.log('else2')
         prevNumpadCell.classList.remove('selected-cell')
-        e.target.classList.add('selected-cell')
+        target.classList.add('selected-cell')
       }
     } else {
-      e.target.classList.add('selected-cell')
+      console.log('else1')
+      target.classList.add('selected-cell')
     }
-    setPrevNumpadCell(e.target)
-    e.target.classList.contains('selected-cell') ? setSelectedNumber(e.target.innerText) : setSelectedNumber('')
+    setPrevNumpadCell(target)
+    target.classList.contains('selected-cell') ? setSelectedNumber(target.innerText) : setSelectedNumber('')
   }
 
   const handleClickSudokuGridCell = (e) => {
@@ -174,6 +191,9 @@ const SudokuGrid = () => {
     const arrSudoku = sudokuUtils.sudoku.board_string_to_grid(stringSudoku)
     setSudokuGrid(arrSudoku)
 
+    // reset candidate
+    resetCandidates()
+
     const originalGridPosition = []
     arrSudoku.flat(Infinity).forEach((value, i) => {
       if (value !== '.') {
@@ -229,34 +249,36 @@ const SudokuGrid = () => {
       </div>
       <div className="option-container">
         <div className="option-button">
+          <div className="numpad-container">
+            <table className="numpad">
+              {numpad.map((row, rowIndex) => {
+                return (
+                  <tbody key={rowIndex}>
+                    <tr>
+                      {row.map((cell, colIndex) => {
+                        return (
+                          <td key={colIndex} onClick={handleClickNumpadCell}>
+                            {cell}
+                          </td>
+                        )
+                      })}
+                    </tr>
+                  </tbody>
+                )
+              })}
+            </table>
+          </div>
+
           <button type="button" onMouseDown={checkCompletedSudoku} onMouseUp={stopCheckCompletedSudoku}>
             Check
-          </button>
-          <button type="button" onClick={getCandidates}>
-            Auto Candidate Mode
           </button>
           <button type="button" onClick={newGame}>
             New game
           </button>
-        </div>
-        <div className="numpad-container">
-          <table className="numpad">
-            {numpad.map((row, rowIndex) => {
-              return (
-                <tbody key={rowIndex}>
-                  <tr>
-                    {row.map((cell, colIndex) => {
-                      return (
-                        <td key={colIndex} onClick={handleClickNumpadCell}>
-                          {cell === '.' ? undefined : cell}
-                        </td>
-                      )
-                    })}
-                  </tr>
-                </tbody>
-              )
-            })}
-          </table>
+          <br />
+
+          <input type="checkbox" name="candidate" id="candidate" onChange={getCandidates} />
+          <label htmlFor="candidate">Auto Candidate Mode</label>
         </div>
       </div>
       {isPopup && <CompletePopup setIsPopup={setIsPopup} newGame={newGame} />}
